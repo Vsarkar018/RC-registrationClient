f2b6202ce0cbd8d31866ec4416c9c5abaeff1ed7
Vsarkar018 <sarkarvishal018@gmail.com>
2023-09-20 00:25:32 +0530

    Registration Closed

---
+++ src/Closed.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Closed = () => {
+  return (
+    <div
+      style={{
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1>Registration are closed...</h1>
+      <img style={{ width: "60%" }} src="/5.svg" alt="" />
+      <h2>
+        Robtics Club <span style={{ color: "#00c9ff" }}>AKGEC</span>
+      </h2>
+    </div>
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-
+import { Closed } from "./Closed.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Closed />
   </React.StrictMode>
 );
